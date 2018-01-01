c8fa0619984626cec75f9ee91a7ec32f716adce1
Marc Hoersken <info@marc-hoersken.de>
2018-01-01 17:47:12 +0100

    Initial commit

---
+++ Dockerfile
@@ -0,0 +1,23 @@
+FROM mback2k/debian:stretch-backports
+
+MAINTAINER Marc Hoersken "info@marc-hoersken.de"
+
+RUN apt-get update && \
+    apt-get install -y --no-install-recommends \
+        net-tools && \
+    apt-get install -y --no-install-recommends \
+        -t stretch-backports openvpn && \
+    apt-get clean
+
+RUN adduser --disabled-password --disabled-login --system --uid 1194 --home /etc/openvpn openvpn
+
+EXPOSE 1194/tcp 1194/udp
+
+VOLUME /etc/openvpn
+
+ENV OPENVPN_USER=openvpn
+ENV OPENVPN_NETNAME=
+
+ADD docker-entrypoint.d/ /run/docker-entrypoint.d/
+
+CMD ["/usr/local/sbin/openvpn"]
---
+++ docker-entrypoint.d/01-openvpn-netname.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+set -e
+
+# check environment variable OPENVPN_USER
+if [ -z "${OPENVPN_USER}" ]; then
+    echo "Environment variable OPENVPN_USER is required"
+    exit 1
+fi
+
+# check environment variable OPENVPN_NETNAME
+if [ -z "${OPENVPN_NETNAME}" ]; then
+    echo "Environment variable OPENVPN_NETNAME is required"
+    exit 2
+fi
+
+echo "#!/bin/sh" > /usr/local/sbin/openvpn
+echo "exec /usr/sbin/openvpn --user ${OPENVPN_USER} --daemon ovpn-${OPENVPN_NETNAME} --status /run/openvpn/${OPENVPN_NETNAME}.status 10 --cd /etc/openvpn --config /etc/openvpn/${OPENVPN_NETNAME}.conf" >> /usr/local/sbin/openvpn
+
+chown --reference=/usr/sbin/openvpn /usr/local/sbin/openvpn
+chmod --reference=/usr/sbin/openvpn /usr/local/sbin/openvpn
