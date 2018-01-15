381c336c8b740dce57989c67b94daffadee60483
Marc Hoersken <info@marc-hoersken.de>
2018-01-15 22:00:29 +0100

    Fix nsupdate key pair generation script

--- docker-entrypoint.d/02-nsupdate-keygen.sh
+++ docker-entrypoint.d/02-nsupdate-keygen.sh
@@ -13,12 +13,18 @@ if [ -z "${OPENVPN_NETNAME}" ]; then
     exit 2
 fi
 
-mkdir /etc/nsupdate
+mkdir -p /etc/nsupdate
+cd /etc/nsupdate
+
 chown ${OPENVPN_USER} /etc/nsupdate
 chmod 755 /etc/nsupdate
 
-OPENVPN_KEYNAME=`dnssec-keygen -a HMAC-MD5 -b 512 -n USER /etc/nsupdate/${OPENVPN_NETNAME}-nsupdate`
+OPENVPN_KEYNAME=`dnssec-keygen -a HMAC-MD5 -b 512 -n USER ${OPENVPN_NETNAME}-nsupdate`
 
-chown ${OPENVPN_USER} ${OPENVPN_KEYNAME}.private ${OPENVPN_KEYNAME}.key
+chown ${OPENVPN_USER} ${OPENVPN_KEYNAME}.private
+chown ${OPENVPN_USER} ${OPENVPN_KEYNAME}.key
 chmod 600 ${OPENVPN_KEYNAME}.private
 chmod 644 ${OPENVPN_KEYNAME}.key
+
+ln -s ${OPENVPN_KEYNAME}.private ${OPENVPN_NETNAME}.private
+ln -s ${OPENVPN_KEYNAME}.key ${OPENVPN_NETNAME}.key
